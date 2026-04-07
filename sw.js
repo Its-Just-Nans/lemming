efbdfbde991ddd192f752b7d1fa274c9520ccde0
n4n5 <its.just.n4n5@gmail.com>
2026-04-06 20:46:05 -0600

    sync

---
+++ sw.js
@@ -0,0 +1,20 @@
+var cacheName = "lemming-pwa";
+var filesToCache = ["./", "./index.html", "./lemming.js", "./lemming.wasm"];
+
+async function networkFirst(request) {
+    try {
+        const networkResponse = await fetch(request);
+        if (networkResponse.ok) {
+            const cache = await caches.open(cacheName);
+            cache.put(request, networkResponse.clone());
+        }
+        return networkResponse;
+    } catch (error) {
+        const cachedResponse = await caches.match(request);
+        return cachedResponse || Response.error();
+    }
+}
+
+self.addEventListener("fetch", (event) => {
+    event.respondWith(networkFirst(event.request));
+});
