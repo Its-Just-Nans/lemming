53c290e39e6f62bd1ba02452ee2b96dba361c768
n4n5 <its.just.n4n5@gmail.com>
2026-05-17 16:55:57 +0200

    update assets

---
+++ assets/gen_assets.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+IMG_NAME="lemming.svg"
+
+if [ ! -f "lemming.png" ]; then
+    convert -background none lemming.svg -resize 512x512 lemming.png
+fi
+if [ ! -f "icon_ios_touch_192.png" ]; then
+    convert -background none lemming.svg -resize 192x192 icon_ios_touch_192.png
+fi
+if [ ! -f "icon-256.png" ]; then
+    convert -background none lemming.svg -resize 256x256 icon-256.png
+fi
+if [ ! -f "icon-1024.png" ]; then
+    convert -background none lemming.svg -resize 1024x1024 icon-1024.png
+fi
+if [ ! -f "maskable_icon_x512.png" ]; then
+    convert -background none lemming.svg -resize 512x512 maskable_icon_x512.png
+fi
+
+# https://golb.n4n5.dev/utils-linux.html#one-liner-faviconico-generator
+
+TO_ICONIFY=lemming.svg
+if [ ! -f "favicon.ico" ]; then
+    for i in 48 96 144 192; do convert -background none $TO_ICONIFY -resize ${i}x${i} favicon-${i}x${i}.png; done
+    convert -background none favicon-* favicon.ico
+    rm favicon-*
+fi
+
+for i in *.svg; do
+    if [ ! -f "${i%.svg}.png" ]; then
+        convert -background none "$i" -resize 64x64 "${i%.svg}.png"
+    fi
+done
